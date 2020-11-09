62dfa7ef58ea42af4437e95cc6e6f3ba3ff85dbc
Abimael Rodrigues Sergio <abimaelr.sergio@gmail.com>
2020-11-09 20:00:15 -0300

    Primeiro Commit

--- src/main/java/br/com/estacio/ads/ProfissaoTestDrive.java
+++ src/main/java/br/com/estacio/ads/ProfissaoTestDrive.java
@@ -6,8 +6,8 @@ import java.util.Comparator;
 
 public class ProfissaoTestDrive {
     public static void main(String[] args) {
-        Profissao p1 = new Profissao("Pedreiro", 20);
-        Profissao p2 = new Profissao("Arquiteto", 2);
+        Profissao p1 = new Profissao("Pedreiro", 2);
+        Profissao p2 = new Profissao("Arquiteto", 20);
         Profissao p3 = new Profissao("Marceneiro", 7);
         ArrayList<Profissao> profissoes = new ArrayList<Profissao>();
         profissoes.add(p1);
@@ -16,7 +16,5 @@ public class ProfissaoTestDrive {
         System.out.println(profissoes);
         Collections.sort(profissoes, Comparator.comparing(Profissao::getAnosExperiencia));
         System.out.println(profissoes);
-
-        p1.getAnosExperiencia();
     }
 }
